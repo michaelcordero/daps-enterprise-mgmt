844745c8089c8b05bbbbc33adfa826b6ff2653e7
michaelcordero <michaelpetercordero@gmail.com>
2020-02-26 02:03:24 -0600

    committing what is the beginning of a batch process program, in case the data has to be translated to CSV first and I have to manually migrate the new data every time.

---
+++ src/utilities/H2exCSV.kt
@@ -0,0 +1,67 @@
+package com.daps.ent.utilities
+
+import io.ktor.utils.io.errors.IOException
+import org.h2.tools.Csv
+import java.io.File
+import java.sql.ResultSet
+import java.sql.ResultSetMetaData
+import kotlin.system.exitProcess
+
+class H2exCSV {
+    fun process(directory: File): Unit {
+        println("Directory passed in: ${directory}")
+        val files: List<File> = directory.listFiles()?.filterNotNull().orEmpty()
+        for (f in files){
+//            if (f.name.equals("DAPSaddress.csv")) {
+            if (f.name.endsWith(".csv")){
+                println("=======================================")
+                println("Processing... ${f.name}")
+                val result_set: ResultSet = Csv().read("${directory}/${f.name}", null, null)
+                val meta: ResultSetMetaData = result_set.metaData
+                println("=======================================")
+                while (result_set.next()) {
+                    // Print the column labels once
+                    if ( result_set.row.equals(1) ){
+                        for (i in 0..meta.columnCount-1) {
+                            print(meta.getColumnLabel(i+1)+"  \t")
+                        }
+                    }
+                    println()
+                    // Print each of the rows
+                    for (i in 0..meta.columnCount-1) {
+                        print(result_set.getString(i+1)+"   \t")
+                    }
+                    println()
+                }
+                println("=======================================")
+                result_set.close()
+            }
+//            }
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    println("=======================================")
+    println("=============CSV Processor=============")
+    println("=======================================")
+    try {
+        val path: String
+        if ( args.isEmpty()){
+            println("No root directory passed in. Exiting process...")
+            exitProcess(-1)
+        } else {
+            path = args[0]
+        }
+        val file = File(path)
+        if (!file.isDirectory){
+            throw IOException("File must be a directory.")
+        }
+        val h2csv = H2exCSV()
+        h2csv.process(file)
+    } catch (e: Exception){
+        println("Error processing files: ${e.cause}")
+    } finally {
+        println("===================================")
+    }
+}
