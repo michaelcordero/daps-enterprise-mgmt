04bfc7da01735763844328e39edafee1125963b4
michaelcordero <michaelpetercordero@gmail.com>
2020-03-22 15:14:08 -0500

    * committing code that will output progress updates for the migration tool H2exCSV.kt. * it appears that Gradle's JavaExec task does not honor carriage returns, so I'll investigate that further. For now, simply running the Jar task and executing the migration as a normal Java process will produced desired output.

--- src/utilities/H2exCSV.kt
+++ src/utilities/H2exCSV.kt
@@ -19,46 +19,124 @@ import kotlin.system.exitProcess
  * app's database. Thus the purpose of this program is to serve as a data processor and batch database updater.
  */
 class H2exCSV {
+
+    fun <T> doSomething(data: T) {
+        // pretend like we're processing data...
+    }
+
+    fun count_rows(resultSet: ResultSet): Int {
+        var counter = 0
+        while (resultSet.next()) {
+            counter++
+        }
+        return counter
+    }
+
     fun process(directory: File) {
         try {
 //            val dao: LocalDataService = LocalDataService()
             println("Directory passed in: $directory")
             val files: List<File> = directory.listFiles()?.filterNotNull().orEmpty()
-            for (f in files){
+            for (f in files) {
                 if (f.name.endsWith(".csv")) {
                     println("=======================================")
-                    println("Processing... ${f.name}")
+                    println("Processing: ${f.name}...")
+                    val counter_set: ResultSet = Csv().read("${directory}/${f.name}", null, null)
+                    val total_rows: Int = count_rows(counter_set)
+                    counter_set.close()
                     val result_set: ResultSet = Csv().read("${directory}/${f.name}", null, null)
+                    var cursor = 0
                     println("=======================================")
                     while (result_set.next()) {
-                            when(f.name) {
-                                "AccountRepDropDown.csv" -> println(AccountRep(result_set))
-                                "BillTypeDropDown.csv" -> println(BillType(result_set))
-                                "Billing.csv" -> println(Billing(result_set)) //application.dao.createBilling(Billing(result_set))
-                                "ClientFile.csv" -> println(ClientFile(result_set))
-                                "ClientNotes.csv" -> println(ClientNotes(result_set))
-                                "ClientPermNotes.csv" -> println(ClientPermNotes(result_set))
-                                "DAPS Staff Messages.csv" -> println(DAPSStaffMessages(result_set))
-                                "DAPSaddress.csv" -> println(DAPSAddress(result_set))
-                                "DAPSstaff.csv" -> println(DAPSStaff(result_set))
-                                "InterviewGuide.csv" -> println(InterviewGuide(result_set))
-                                "JobFunctionDropDown.csv" -> println(JobFunction(result_set))
-                                "Paste Errors.csv" -> println(PasteErrors(result_set))
-                                "Payment.csv" -> println(Payment(result_set))
-                                "PermNotes.csv" -> println(PermNotes(result_set))
-                                "PermReqNotes.csv" -> println(PermReqNotes(result_set))
-                                "TempNotes.csv" -> println(TempNotes(result_set))
-                                "Temps.csv" -> println(Temps(result_set))
-                                "TempsAvail4Work.csv" -> println(TempsAvail4Work(result_set))
-                                "WOnotes.csv" -> println(WONotes(result_set))
-                                "WorkOrder.csv" -> println(WorkOrder(result_set))
+                        when (f.name) {
+                            "AccountRepDropDown.csv" -> doSomething(AccountRep(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "BillTypeDropDown.csv" -> doSomething(BillType(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "Billing.csv" -> doSomething(Billing(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "ClientFile.csv" -> doSomething(ClientFile(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "ClientNotes.csv" -> doSomething(ClientNotes(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "ClientPermNotes.csv" -> doSomething(ClientPermNotes(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "DAPS Staff Messages.csv" -> doSomething(DAPSStaffMessages(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "DAPSaddress.csv" -> doSomething(DAPSAddress(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "DAPSstaff.csv" -> doSomething(DAPSStaff(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "InterviewGuide.csv" -> doSomething(InterviewGuide(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "JobFunctionDropDown.csv" -> doSomething(JobFunction(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+
+                            }
+                            "Paste Errors.csv" -> doSomething(PasteErrors(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "Payment.csv" -> doSomething(Payment(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "PermNotes.csv" -> doSomething(PermNotes(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "PermReqNotes.csv" -> doSomething(PermReqNotes(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "TempNotes.csv" -> doSomething(TempNotes(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "Temps.csv" -> doSomething(Temps(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "TempsAvail4Work.csv" -> doSomething(TempsAvail4Work(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "WOnotes.csv" -> doSomething(WONotes(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
+                            "WorkOrder.csv" -> doSomething(WorkOrder(result_set)).also {
+                                cursor++
+                                print("${cursor}/${total_rows}\r")
+                            }
                         }
                     }
                 }
             }
         } catch (e: Exception) {
             println("Error occurred during migration:")
-            println(e.message)
+            println(e)
         } finally {
 //            dao.close()
         }
@@ -71,19 +149,19 @@ fun main(args: Array<String>) {
     println("=======================================")
     try {
         val path: String
-        if ( args.isEmpty()){
+        if (args.isEmpty()) {
             println("No root directory passed in. Exiting process...")
             exitProcess(-1)
         } else {
             path = args[0]
         }
         val file = File(path)
-        if (!file.isDirectory){
+        if (!file.isDirectory) {
             throw IOException("File must be a directory.")
         }
         val h2csv = H2exCSV()
         h2csv.process(file)
-    } catch (e: Exception){
+    } catch (e: Exception) {
         println("Error processing files: ${e.cause}")
     } finally {
         println("===================================")
