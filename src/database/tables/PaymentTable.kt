66459258aa9ae605c4897d86020827ff58c7da07
michaelcordero <michaelpetercordero@gmail.com>
2020-03-28 20:27:28 -0500

    * fixed null errors

--- src/database/tables/PaymentTable.kt
+++ src/database/tables/PaymentTable.kt
@@ -5,9 +5,9 @@ import org.jetbrains.exposed.sql.Table
 
 object PaymentTable: Table() {
     val client_num = integer("Client#")
-    val pmt_type = text("PmtType")
+    val pmt_type = text("PmtType").nullable()
     val ref_num = varchar("Ref#", 20)
     override val primaryKey = PrimaryKey(client_num, ref_num)
-    val pmt_date = realtimestamp("PmtDate")
-    val amount = double("Amount" )
+    val pmt_date = realtimestamp("PmtDate").nullable()
+    val amount = double("Amount" ).nullable()
 }
--- src/model/Payment.kt
+++ src/model/Payment.kt
@@ -5,9 +5,9 @@ import java.io.Serializable
 import java.sql.ResultSet
 import java.sql.Timestamp
 
-data class Payment(val client_num: Int, val pmt_type: String,
-                   val ref_num: String, val pmt_date: Timestamp,
-                   val amount: Double) : Serializable {
+data class Payment(val client_num: Int, val pmt_type: String?,
+                   val ref_num: String, val pmt_date: Timestamp?,
+                   val amount: Double?) : Serializable {
     constructor(result_set: ResultSet) : this (
         result_set.getInt(PaymentTable.client_num.name),
         result_set.getString(PaymentTable.pmt_type.name),
