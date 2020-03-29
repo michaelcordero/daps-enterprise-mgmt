e0a5aeae5b06039225080b293486dbbbed71a0ea
michaelcordero <michaelpetercordero@gmail.com>
2020-03-28 20:40:01 -0500

    * making tables more flexible

--- src/database/tables/PaymentTable.kt
+++ src/database/tables/PaymentTable.kt
@@ -4,9 +4,9 @@ import database.realtimestamp
 import org.jetbrains.exposed.sql.Table
 
 object PaymentTable: Table() {
-    val client_num = integer("Client#")
+    val client_num = integer("Client#").nullable()
     val pmt_type = text("PmtType").nullable()
-    val ref_num = varchar("Ref#", 20)
+    val ref_num = varchar("Ref#", 20).nullable()
     override val primaryKey = PrimaryKey(client_num, ref_num)
     val pmt_date = realtimestamp("PmtDate").nullable()
     val amount = double("Amount" ).nullable()
--- src/model/Payment.kt
+++ src/model/Payment.kt
@@ -5,8 +5,8 @@ import java.io.Serializable
 import java.sql.ResultSet
 import java.sql.Timestamp
 
-data class Payment(val client_num: Int, val pmt_type: String?,
-                   val ref_num: String, val pmt_date: Timestamp?,
+data class Payment(val client_num: Int?, val pmt_type: String?,
+                   val ref_num: String?, val pmt_date: Timestamp?,
                    val amount: Double?) : Serializable {
     constructor(result_set: ResultSet) : this (
         result_set.getInt(PaymentTable.client_num.name),
