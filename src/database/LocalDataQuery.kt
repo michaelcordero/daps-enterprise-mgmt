bba8536d4c543de3dce3efab3f5aef1e834ef744
michaelcordero <michaelpetercordero@gmail.com>
2020-03-28 19:18:09 -0500

    * accounting for new tables

--- src/database/LocalDataQuery.kt
+++ src/database/LocalDataQuery.kt
@@ -1,7 +1,6 @@
 package database
 import database.queries.DataQuery
-import database.tables.BillingTable
-import database.tables.UsersTable
+import database.tables.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -13,7 +12,10 @@ class LocalDataQuery: DataQuery {
     init {
         db = Database.connect(dp.pool)
         transaction(db) {
-           SchemaUtils.create(UsersTable,BillingTable)
+           SchemaUtils.create(AccountRepDropDownTable,BillingTable,BillTypeDropDownTable, ClientFileTable, ClientNotesTable,
+           ClientPermNotesTable, DAPSAddressTable, DAPSStaffMessagesTable, DAPSStaffTable, InterviewGuideTable,
+           JobFunctionDropDownTable, PasteErrorsTable, PasteErrorsTable, PaymentTable, PermNotesTable, PermReqNotesTable,
+           TempNotesTable, TempsAvail4WorkTable, TempsTable,  UsersTable, WONotesTable, WorkOrderTable)
         }
     }
 
