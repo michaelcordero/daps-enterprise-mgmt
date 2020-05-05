90e35abd3f82c3339c2d9783f035a877c250f926
michaelcordero <michaelpetercordero@gmail.com>
2020-05-05 18:03:03 -0500

    * paste errors can't be fixed... :/

--- src/database/tables/PasteErrorsTable.kt
+++ src/database/tables/PasteErrorsTable.kt
@@ -7,7 +7,7 @@ object PasteErrorsTable: Table() {
     val client_num = integer("Client#")
     val pmt_type = text("PmtType").nullable()
     val ref_num = text("Ref#")
-    override val primaryKey = PrimaryKey(ref_num, name = "Ref#")
+//    override val primaryKey = PrimaryKey(ref_num, name = "Ref#")
     val pmt_date = realtimestamp("PmtDate").nullable()
     val amount = double("Amount").nullable()
 }
