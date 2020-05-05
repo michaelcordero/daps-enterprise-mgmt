5c762dd1c4f20ab908a1844e931612cd42fb0b77
michaelcordero <michaelpetercordero@gmail.com>
2020-05-05 17:29:54 -0500

    * added primary keys so these tables queries won't break

--- src/database/tables/PasteErrorsTable.kt
+++ src/database/tables/PasteErrorsTable.kt
@@ -5,6 +5,7 @@ import org.jetbrains.exposed.sql.Table
 
 object PasteErrorsTable: Table() {
     val client_num = integer("Client#")
+    override val primaryKey = PrimaryKey(ClientFileTable.client_num, name = "Client#")
     val pmt_type = text("PmtType").nullable()
     val ref_num = text("Ref#").nullable()
     val pmt_date = realtimestamp("PmtDate").nullable()
