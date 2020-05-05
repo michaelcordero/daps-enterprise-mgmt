552329f08fd41c8f92af6d13b77c3a6071336664
michaelcordero <michaelpetercordero@gmail.com>
2020-05-05 17:47:19 -0500

    * had the PasteErrors.kt primary key table wrong

--- src/database/tables/PasteErrorsTable.kt
+++ src/database/tables/PasteErrorsTable.kt
@@ -5,9 +5,9 @@ import org.jetbrains.exposed.sql.Table
 
 object PasteErrorsTable: Table() {
     val client_num = integer("Client#")
-    override val primaryKey = PrimaryKey(ClientFileTable.client_num, name = "Client#")
     val pmt_type = text("PmtType").nullable()
-    val ref_num = text("Ref#").nullable()
+    val ref_num = text("Ref#")
+    override val primaryKey = PrimaryKey(ref_num, name = "Ref#")
     val pmt_date = realtimestamp("PmtDate").nullable()
     val amount = double("Amount").nullable()
 }
--- src/model/PasteErrors.kt
+++ src/model/PasteErrors.kt
@@ -11,7 +11,8 @@ data class PasteErrors
     @JsonCreator constructor(
         @JsonProperty(value = "client_num", required = true) val client_num: Int,
         val pmt_type: String?,
-        val ref_num: String?, val pmt_date: Timestamp?,
+        @JsonProperty(value = "ref_num", required = true)
+        val ref_num: String, val pmt_date: Timestamp?,
         val amount: Double?) : Serializable {
     constructor(result_set: ResultSet) : this (
         result_set.getInt(PasteErrorsTable.client_num.name),
