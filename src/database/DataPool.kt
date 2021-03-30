cefc2a1cb4f23d42ab0f9eb654c8c007a441af04
michaelcordero <michaelpetercordero@gmail.com>
2021-03-29 20:50:16 -0400

    * changing DB directory to current

--- src/database/DataPool.kt
+++ src/database/DataPool.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 class DataPool {
     // File where the database is to be stored.
-    private val database_directory: String = "build/install/daps-enterprise-mgmt/db"
+    private val database_directory: String = "."
     val dir: File = File( database_directory )
     // Pool of JDBC Connections used
     val pool: HikariDataSource = HikariDataSource().apply {
